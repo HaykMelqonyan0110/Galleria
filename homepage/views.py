78e467a57e43fd78565aefedf653e615c6829ef1
MelineGogjyan <96750352+MelineGogjyan@users.noreply.github.com>
2023-02-26 23:52:08 +0400

    ավելացրածա bace.htmը

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-def base(request):
-    return render(request, 'homepage/base.html')
+def home_page_view(request):
+    return render(request, 'homepage/home.html')
