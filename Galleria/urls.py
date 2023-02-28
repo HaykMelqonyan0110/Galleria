597a9b06e6b73e5f54e98eefe0238ad6b62977ab
HaykMelqonyan0110 <hayk.melqonyan0110@gmail.com>
2023-02-28 21:49:43 +0400

    Fixed some bugs

--- Galleria/urls.py
+++ Galleria/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views
+from user import views as reg
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("homepage.urls")),
-    # path('login/', views.LoginView(template_name='user/login.html'), name='login'),
-    # path('logout/', views.LogoutView(template_name='user/logout.html'), name='logout')
+    path('login/', views.LoginView(template_name='user/login.html'), name='login'),
+    path('logout/', views.LogoutView(template_name='user/logout.html'), name='logout'),
+    path('register/', reg.register, name="register"),
 ]
