a3a7436b13bf4fb894bed8b2d4a8c8f1690ffafa
HaykMelqonyan0110 <hayk.melqonyan0110@gmail.com>
2023-03-01 17:26:07 +0400

    Added some paths in urls.py

--- Galleria/urls.py
+++ Galleria/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
+from user_profile import views as profile_views
 from django.urls import path, include
 from django.contrib.auth import views
-from user import views as reg
+from user import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("homepage.urls")),
-    path('login/', views.LoginView(template_name='user/login.html'), name='login'),
-    path('logout/', views.LogoutView(template_name='user/logout.html'), name='logout'),
-    path('register/', reg.register, name="register"),
+    path('login/', views.LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout/', views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
+    path('register/', user_views.register, name="register"),
+    path('profile/', profile_views.profile, name='profile')
 ]
